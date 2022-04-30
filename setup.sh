b96af063b58eec4503c6c38d95df8212f93ea3ba
Marco Cecotti <12512866+MarcoCecotti@users.noreply.github.com>
2022-04-30 08:13:14 +0100

    Removed unnecessary steps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Make FS read-only to avoid SD card corruption
-git clone https://gitlab.com/larsfp/rpi-readonly
-cd rpi-readonly
-sudo ./setup.sh -y
-cd ..
+#git clone https://gitlab.com/larsfp/rpi-readonly
+#cd rpi-readonly
+#sudo ./setup.sh -y
+#cd ..
 
 # Prepare system
 sudo apt-get update -y
@@ -20,9 +20,9 @@ sudo cp config.txt /boot/
 sudo cp cmdline.txt /boot/
 
 # Make device identifiable more easily on the network
-sudo apt-get install avahi-daemon -y
-sudo sed -i -- 's/raspberrypi/midihub/g' /etc/hostname /etc/hosts
-sudo hostname midihub
+# sudo apt-get install avahi-daemon -y
+# sudo sed -i -- 's/raspberrypi/midihub/g' /etc/hostname /etc/hosts
+# sudo hostname midihub
 
 # Install MIDI autoconnect script
 sudo cp connectall.rb /usr/local/bin/
@@ -38,23 +38,23 @@ sudo systemctl start midi.service
 sudo apt-get install midisport-firmware -y
 
 # Setup MIDI bluetooth
-git clone https://github.com/oxesoft/bluez
-sudo apt-get install -y autotools-dev libtool autoconf
-sudo apt-get install -y libasound2-dev
-sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
-cd bluez
-./bootstrap
-./configure --enable-midi --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
-make
-sudo make install
-cd ..
-sudo cp 44-bt.rules /etc/udev/rules.d/
-sudo udevadm control --reload
-sudo service udev restart
-sudo cp btmidi.service /lib/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable btmidi.service
-sudo systemctl start btmidi.service
+#git clone https://github.com/oxesoft/bluez
+#sudo apt-get install -y autotools-dev libtool autoconf
+#sudo apt-get install -y libasound2-dev
+#sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+#cd bluez
+#./bootstrap
+#./configure --enable-midi --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
+#make
+#sudo make install
+#cd ..
+#sudo cp 44-bt.rules /etc/udev/rules.d/
+#sudo udevadm control --reload
+#sudo service udev restart
+#sudo cp btmidi.service /lib/systemd/system/
+#sudo systemctl daemon-reload
+#sudo systemctl enable btmidi.service
+#sudo systemctl start btmidi.service
 
 # Create alias to show connected devices
 echo >> ~/.bashrc
@@ -68,9 +68,9 @@ echo >> ~/.bashrc
 
 # Turn on read-only mode
 # Use command "rw" to enable writes again
-ro
+#ro
 
-echo "The system will now reboot in read-only mode."
-echo "Use command \"rw\" to enable writes again."
-read -p "Press [enter] to reboot"
+#echo "The system will now reboot in read-only mode."
+#echo "Use command \"rw\" to enable writes again."
+#read -p "Press [enter] to reboot"
 sudo reboot
